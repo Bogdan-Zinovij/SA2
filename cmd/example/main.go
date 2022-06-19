3fb99a9911d556234ca66af545e8af6e34533fe2
Bogdan-Zinovij <76776361+Bogdan-Zinovij@users.noreply.github.com>
2022-06-19 13:11:19 +0300

    Initial commit

---
+++ cmd/example/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	lab2 "github.com/roman-mazur/architecture-lab-2"
+)
+
+var (
+	inputExpression = flag.String("e", "", "Expression to compute")
+	// TODO: Add other flags support for input and output configuration.
+)
+
+func main() {
+	flag.Parse()
+
+	// TODO: Change this to accept input from the command line arguments as described in the task and
+	//       output the results using the ComputeHandler instance.
+	//       handler := &lab2.ComputeHandler{
+	//           Input: {construct io.Reader according the command line parameters},
+	//           Output: {construct io.Writer according the command line parameters},
+	//       }
+	//       err := handler.Compute()
+
+	res, _ := lab2.PrefixToPostfix("+ 2 2")
+	fmt.Println(res)
+}
---
+++ implementation.go
@@ -0,0 +1,9 @@
+package lab2
+
+import "fmt"
+
+// TODO: document this function.
+// PrefixToPostfix converts
+func PrefixToPostfix(input string) (string, error) {
+	return "TODO", fmt.Errorf("TODO")
+}
