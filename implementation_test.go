78970ecba270c25ec34f0fac3c8dd82766acfdbc
VasylKhrapko <khrapko2002@gmail.com>
2022-06-21 12:40:04 +0300

    Broken tests to check if the build fails

--- implementation_test.go
+++ implementation_test.go
@@ -11,8 +11,8 @@ import (
 func TestPostfixToPrefix(t *testing.T) {
 	cases := map[string]string{
 		"1 2 +":                                 "+ 1 2",
-		"1 2 3 * +":                             "+ 1 * 2 3",
-		"4 2 / 5 4 * 3 + /":                     "/ / 4 2 + * 5 4 3",
+		"1 2 3 * ":                              "+ 1 * 2 3",
+		"4 2 / 5 4 * 3 + ":                      "/ / 4 2 + * 5 4 3",
 		"3 4 + 1 - 6 5 - 2.0 8 7 + * / ^":       "^ - + 3 4 1 / - 6 5 * 2.0 + 8 7",
 		"1 2 3 + + 4 - 5 + 8 ^ 3 7 * ^ 2 2 + -": "- ^ ^ + - + 1 + 2 3 4 5 8 * 3 7 + 2 2",
 		"":                                      "validation error: expression is not specified",
